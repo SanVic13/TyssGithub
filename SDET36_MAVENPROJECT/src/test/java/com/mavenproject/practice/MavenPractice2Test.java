bb1746ddb802bce8800847ed434d8dd550872864
sandeepPandey <pandeysandeep910@gmail.com>
2022-07-19 11:44:29 +0530

    Github practice

---
+++ SDET36_MAVENPROJECT/src/test/java/com/mavenproject/practice/MavenPractice2Test.java
@@ -0,0 +1,22 @@
+package com.mavenproject.practice;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class MavenPractice2Test {
+
+	@Test
+	public void mavenPractice2Test1() {
+		Reporter.log("mavenPractice2Test1",true);
+	}
+
+	@Test
+	public void mavenPractice2Test2() {
+		Reporter.log("mavenPractice2Test2",true);
+	}
+
+	@Test
+	public void mavenPractice2Test3() {
+		Reporter.log("mavenPractice2Test3",true);
+	}
+}
